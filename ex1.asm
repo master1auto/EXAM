a04c4b769fa522ca977cb94f145b44bb3ca2e472
master1auto_ouargla <microprocessor.m1auto@gmail.com>
2016-01-15 22:17:09 +0100

    Update ex1.asm

--- ex1.asm
+++ ex1.asm
@@ -1,1 +1,34 @@
+;configure all ports as output
+mov dx,606h
+mov al,80h
+out dx,al 
+;PC4 is on
+mov dx,604h
+cc:mov al,10h
+out dx,al 
+call delay3s
+;PC5 is on 
+mov al,20h
+out dx,al 
+call delay3s
+;PC6 is on 
+mov al,40h
+out dx,al 
+call delay3s
+;PC7 is on 
+mov al,80h
+out dx,al 
+call delay3s 
+jmp cc
+hlt
 
+delay3s proc
+mov bx,100
+ff:dec bx
+mov cx,7500
+tt:nop
+loop tt
+cmp bx,0
+jnz ff
+ret
+delay3s endp    
